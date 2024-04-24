61bee4320120279753dbc886e651ab44c1ffdaf5
Fihliwe <siyabulelmncube@gmail.com>
2024-04-24 11:32:06 +0000

    Parameter defaults

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,11 +1,1 @@
-export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 190) {
-  // if (expansion1989 === undefined) {
-  // expansion1989 = 89;
-  return initialNumber + expansion1989 + expansion2019;
-  // }
-
-  // if (expansion2019 === undefined) {
-  // expansion2019 = 19;
-  // }
-  // return initialNumber + expansion1989 + expansion2019;
-}
+export default getSumOfHoods = (initialNumber, expansion1989 = 89, expansion2019 = 190) => { return initialNumber + expansion1989 + expansion2019;}
